3e8ec2fa9d916d2459d273e030c07ebd49cf4b43
Shawn de Bie <shawn.debie@gamil.com>
2026-02-16 16:29:57 +0200

    added host config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+import dynamicImport from 'vite-plugin-dynamic-import'
 
 export default defineConfig({
   plugins: [react(), dynamicImport()],
