569826ec5ec250ecd5ab77a3f31ef1eadd7b7f83
Shawn de Bie <shawn.debie@gamil.com>
2026-03-09 13:58:04 +0200

    Code cleanup

--- src/pages/shop/cart/hook/useAddToCart.ts
+++ src/pages/shop/cart/hook/useAddToCart.ts
@@ -7,7 +7,9 @@ export function useAddToCart() {
     const [createLoading, setCreateLoading] = useState(false);
     const [createError, setCreateError] = useState<Error | null>(null);
 
-    async function addToCart(orderDetail: OrderData): Promise<OrderData> {
+    async function addToCart(orderDetail: {
+        items: { quantity: number; unitPrice: number; variant: string }[]
+    }): Promise<OrderData> {
         console.log("DEBUG:: Creating order: ", orderDetail);
         setCreateLoading(true);
         setCreateError(null);
