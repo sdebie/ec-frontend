35133a2b3b24f43153d072d78b32f11740667376
Shawn de Bie <shawn.debie@gamil.com>
2026-05-28 13:06:18 +0200

    made page size for product shoppimg list 25

--- src/pages/admin/brands/hooks/useBrandList.ts
+++ src/pages/admin/brands/hooks/useBrandList.ts
@@ -4,7 +4,7 @@ import {apiGetAllBrands, apiGetBrandCount} from "@/services/graphql/admin/brand/
 import {Brand} from "@/types/admin/BrandTypes.ts";
 import {FilterRequest} from "@/types/graphql/query.types.ts";
 
-const DEFAULT_PAGE_SIZE = 10;
+const DEFAULT_PAGE_SIZE = 25;
 
 export default function useBrandList() {
 
