33fb6b6bf102493012c1774dbe4c1424de29f00d
Shawn de Bie <shawn.debie@gamil.com>
2026-06-01 14:28:05 +0200

    localhost is working

--- src/constants/api.constant.ts
+++ src/constants/api.constant.ts
@@ -2,5 +2,5 @@ export const TOKEN_TYPE = 'Bearer '
 export const REQUEST_HEADER_AUTH_KEY = 'Authorization'
 export const TOKEN_NAME_IN_STORAGE = 'token'
 
-export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || 'http://localhost:8080/static/images/';
+export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || '/static/images/';
 export const IMAGE_THUMBNAIL_URL = `${IMAGE_BASE_URL}thumbnails/`;
--- vite.config.ts
+++ vite.config.ts
@@ -132,6 +132,11 @@ export default defineConfig(({ mode }) => {
           target: proxyTarget,
           changeOrigin: true,
           secure: false
+        },
+        '/static': {
+          target: proxyTarget,
+          changeOrigin: true,
+          secure: false
         }
       },
       watch: {
