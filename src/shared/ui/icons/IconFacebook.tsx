4eda1d99621e459500b2c999d588ffb8c29d2706
Vanessa De Jager <vanessa.dejager+gonz@gobss.nz>
2026-07-12 21:52:23 +0200

    Refactor social icon components to ensure consistent formatting, improve code readability, and update imports to include explicit `JSX` type.

--- src/shared/ui/icons/IconFacebook.tsx
+++ src/shared/ui/icons/IconFacebook.tsx
@@ -1,9 +1,10 @@
-import type React from 'react'
+import React, {type JSX} from 'react'
 
 type SvgIconProps = React.SVGProps<SVGSVGElement>
 
-export const IconFacebook = ({ width = 24, height = 24, ...props }: SvgIconProps): JSX.Element => (
-  <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" width={width} height={height} {...props}>
-    <path d="M9.101 23.691v-7.98H6.627v-3.667h2.474v-1.58c0-4.085 1.848-5.978 5.858-5.978.401 0 .955.042 1.468.103a8.68 8.68 0 0 1 1.141.195v3.325a8.623 8.623 0 0 0-.653-.036 26.805 26.805 0 0 0-.733-.009c-.707 0-1.259.096-1.675.309a1.686 1.686 0 0 0-.679.622c-.258.42-.374.995-.374 1.752v1.297h3.919l-.405 2.08-.43 1.588h-3.084v8.268a23.159 23.159 0 0 1-3.874-.271z" />
-  </svg>
+export const IconFacebook = ({width = 24, height = 24, ...props}: SvgIconProps): JSX.Element => (
+    <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" width={width} height={height} {...props}>
+        <path
+            d="M9.101 23.691v-7.98H6.627v-3.667h2.474v-1.58c0-4.085 1.848-5.978 5.858-5.978.401 0 .955.042 1.468.103a8.68 8.68 0 0 1 1.141.195v3.325a8.623 8.623 0 0 0-.653-.036 26.805 26.805 0 0 0-.733-.009c-.707 0-1.259.096-1.675.309a1.686 1.686 0 0 0-.679.622c-.258.42-.374.995-.374 1.752v1.297h3.919l-.405 2.08-.43 1.588h-3.084v8.268a23.159 23.159 0 0 1-3.874-.271z"/>
+    </svg>
 )
